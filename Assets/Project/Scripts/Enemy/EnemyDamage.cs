f43c5e4ca8b299f20e3e3b88ce3cefe2addaedd9
OğuzhaN <54539380+oguzhan07@users.noreply.github.com>
2026-09-20 23:25:58 +0300

    güncel

--- Assets/Project/Scripts/Enemy/EnemyDamage.cs
+++ Assets/Project/Scripts/Enemy/EnemyDamage.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -20,14 +21,27 @@ public class EnemyDamage : MonoBehaviour
 
     public void EnemyGetDamage(float amount)
     {
-        
         circleCenter = enemy.transform.position + new Vector3(0, 2);
         print("circle center: " + circleCenter);
         print("enemy pos: " + enemy.transform.position);
-        randomPos = new Vector2(Random.Range(circleCenter.x - circleRadius, circleCenter.x + circleRadius),
+        randomPos = new Vector3(Random.Range(circleCenter.x - circleRadius, circleCenter.x + circleRadius),
             Random.Range(circleCenter.y - circleRadius, circleCenter.y + circleRadius));
-        Instantiate(enemyDamageNumber);
+        Instantiate(enemyDamageNumber, randomPos, new Quaternion());
         textMeshPro.text = amount.ToString();
-        textMeshPro.transform.position = randomPos;
+        enemyDamageNumber.transform.position = randomPos;
+
+        /*Color transparentRed = new Color(255, 0, 0, 0);
+        textMeshPro.DOColor(transparentRed, 2f);*/
+        
+        enemyDamageNumber.transform.DOScale(1f, 1f);
+        
+        /*textMeshPro.transform.DOMoveY(textMeshPro.transform.position.y + 0.5f, 1f)
+            .SetEase(Ease.Linear);*/
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(circleCenter, circleRadius);
     }
 }
